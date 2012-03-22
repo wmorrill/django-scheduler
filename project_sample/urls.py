e6c57d330ca09579514f6d2655d9438464b82f61
Benjamin Joseph Dag <bjdag1234@gmail.com>
2012-03-22 23:01:43 +0800

    Merged with Chrislene and Lou's code

--- project_sample/urls.py
+++ project_sample/urls.py
@@ -1,20 +1,30 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+from django.contrib.auth.decorators import login_required
+from decorator_include import decorator_include
 
 from django.contrib import admin
 admin.autodiscover()
 
+from schedule import views
+
 urlpatterns = patterns('',
     url(r'^$', direct_to_template,{"template":"homepage.html"}),
-    (r'^schedule/', include('schedule.urls')),
+    (r'^schedule/', decorator_include(login_required, 'schedule.urls')),
+	(r'^accounts/', include('registration.urls')),
+	(r'^search_form_room/$', views.search_form_room),
+	(r'^search_by_room/$', views.search_by_room),
+	(r'^search_form_date/$', views.search_form_date),
+	(r'^search_by_date/$', views.search_by_date),
+	(r'^my_reservations/$', views.my_reservations),
+	(r'^all_reservations/$', views.all_reservations),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+	url(r'^event/delete/(?P<event_id>\d+)/$',
+		'schedule.views.delete_event',
+		name="delete_event"),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
